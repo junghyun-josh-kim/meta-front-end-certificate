99ae12846b97cb5800b56f287a39eb2cc786c623
Josh-Kim-Toronto <jhkim911206@gmail.com>
2022-09-23 20:09:46 -0400

    recapping

--- test.js
+++ test.js
@@ -1,3 +1,8 @@
-function letterFinder (word, match) {
-    for ( var i = 0; i <= word.length; i = i + 1)
-}
+<form>
+   <input type="----" id="alpha" name="letters" value="Alpha">
+   <label for="html">Alpha</label><br>
+   <input type="----" id="beta" name="letters" value="Beta">
+   <label for="css">Beta</label><br>
+   <input type="----" id="gamma" name="letters" value="Gamma">
+   <label for="javascript">Gamma</label>
+</form>
